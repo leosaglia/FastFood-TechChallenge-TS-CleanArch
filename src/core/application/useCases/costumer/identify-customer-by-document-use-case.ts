0e42e9b1a65dbebc821dc16605ac9f0cff31400c
Léo Saglia <leosaglia@hotmail.com>
2024-12-13 20:52:04 -0300

    corrigir use case de cliente para usar interface do gateway

--- src/core/application/useCases/costumer/identify-customer-by-document-use-case.ts
+++ src/core/application/useCases/costumer/identify-customer-by-document-use-case.ts
@@ -1,14 +1,14 @@
 import { Customer } from '@core/enterprise/entities/customer'
 import { Document } from '@core/enterprise/valueObjects/document'
-import { CustomerGateway } from '@infra/gateway/customer-gateway'
 import { CustomerNotFoundError } from '@core/enterprise/custom-exceptions/customer-not-found'
+import { ICustomerGateway } from '@core/application/interfaces/gateway/customer-gateway-interface'
 
 type IdentifyCustomerByDocumentUseCaseResponse = {
   customer: Customer
 }
 
 export class IdentifyCustomerByDocumentUseCase {
-  constructor(private customerGateway: CustomerGateway) {}
+  constructor(private customerGateway: ICustomerGateway) {}
 
   async execute(
     document: string,
